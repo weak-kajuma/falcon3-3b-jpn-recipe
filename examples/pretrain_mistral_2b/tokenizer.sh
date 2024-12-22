2c422ad94209110f961a2ed898a68fc46258f528
ce-lery <celery3110@gmail.com>
2024-12-22 08:53:09 +0000

    feat: update creating tokenizer script

--- examples/pretrain_mistral_2b/tokenizer.sh
+++ examples/pretrain_mistral_2b/tokenizer.sh
@@ -9,29 +9,22 @@ log=results/log/$(basename "$0" .sh)/$(date +%Y%m%d_%H%M%S).log
 exec &> >(tee -a $log)
 set -x
 
-mkdir -p results/preprocess
+mkdir -p results/tokenizer
 
-# if "${CLEAN_CORPUS}"; then
-#     # clean corpus by "corpus_cleaner"
+# clean corpus by "corpus_cleaner"
+#create corpus for torkenizer
+python ../../source/util/extract_text_of_jsonl.py \
+    --input_path="./results/tokenizer/tokenizer_corpus.jsonl" \
+    --output_path="./results/tokenizer/tokenizer_corpus.txt"
 
-#     #create corpus for torkenizer
-#     python ./mistral/util/extract_text_of_jsonl.py \
-#         --input_path="./results/dataset/train.jsonl" \
-#         --output_path="./results/preprocess/tokenizer_corpus.txt"
-
-# else
-#     # create corpus for torkenizer
-#     echo "else"
-# fi
-
+#rm ./results/dataset/tokenizer_corpus.jsonl#
 
 # create tokenizer
-mkdir -p ./results/preprocess/spm
-mkdir -p ./results/preprocess/llamatokenizer
+mkdir -p ./results/tokenizer/spm
+mkdir -p ./results/tokenizer/llamatokenizer
 python ../../source/tokenize/unigram.py \
-    --model_prefix="./results/preprocess/spm" \
-    --output_dir="./results/preprocess/llamatokenizer" \
-    --corpus_path="./results/preprocess/tokenizer_corpus.txt"
-
+    --model_prefix="./results/tokenizer/spm" \
+    --output_dir="./results/tokenizer/llamatokenizer" \
+    --corpus_path="./results/tokenizer/tokenizer_corpus.txt"
 
 set +x
