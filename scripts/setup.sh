3832219d821ebd12d986b8d65a7b8e67484994dd
weak-kajuma <kazuma826826@gmail.com>
2024-12-28 12:08:30 +0000

    a

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 
-pip install git+https://github.com/nhamanasu/transformers.git@schedulefree-radam
-pip install sentencepiece datasets evaluate accelerate==1.1.0 deepspeed
+pip install transformers==4.46.3
+pip install sentencepiece datasets evaluate accelerate==1.2.1
 pip install scikit-learn
 pip install bitsandbytes
 pip install liger_kernel
 pip install schedulefree
-# pip install wheel
-# pip install flash-attn --no-build-isolation
+pip install wheel
+pip install flash-attn --no-build-isolation
--- scripts/train_token.sh
+++ scripts/train_token.sh
@@ -1,30 +1,29 @@
-BATCH_SIZE=1
-GRADIENT_ACCUMULATION_STEPS=128
+BATCH_SIZE=32
+GRADIENT_ACCUMULATION_STEPS=8
 EPOCH=1
 DIR_NAME=patch_pretrain_falcon_3b
-mkdir -p ../results/pretrain/
+mkdir -p ~/falcon3/results/
 
-accelerate launch ../source/train/run_clm_patch.py \
+python ../source/train/run_clm_patch.py \
     --model_type "llama" \
-    --model_name_or_path tiiuae/Falcon3-1B-Base \
-    --dataset_name kajuma/CC-news-2024-July-October-cleaned \
+    --model_name_or_path kajuma/falcon3_3b_patch \
+    --dataset_name kajuma/training_12-23_token \
     --patch_size 4 \
-    --output_dir ./results/pretrain/$DIR_NAME/trial1 \
-    --cache_dir ./results/pretrain/cache/patch_train \
+    --output_dir ~/falcon3/results/ \
+    --cache_dir ./cache/ \
     --do_train \
     --do_eval \
     --prediction_loss_only \
     --remove_unused_columns False \
     --learning_rate 2.0e-4 \
     --weight_decay 0.1 \
-    --adam_beta2 0.95 \
     --num_train_epochs $EPOCH \
-    --logging_dir ./results/pretrain/logs/$DIR_NAME/trial1 \
+    --logging_dir ~/falcon3/results/ \
     --logging_strategy "steps" \
     --logging_steps 10 \
     --save_strategy "steps" \
     --save_steps 1000 \
-    --save_total_limit 3 \
+    --save_total_limit 100000 \
     --lr_scheduler_type constant \
     --per_device_train_batch_size $BATCH_SIZE \
     --per_device_eval_batch_size $BATCH_SIZE \
@@ -33,8 +32,8 @@ accelerate launch ../source/train/run_clm_patch.py \
     --torch_dtype "bfloat16" \
     --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
     --push_to_hub False \
-    --preprocessing_num_workers 8 \
-    --dataloader_num_workers 8 \
+    --preprocessing_num_workers 64 \
+    --dataloader_num_workers 64 \
     --optim "schedule_free_adamw" \
     --attn_implementation "flash_attention_2" \
     # --torch_compile True \
